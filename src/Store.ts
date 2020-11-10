e6817e086c527795da96cba792692a7d07a92b01
Tin La <tinla14@gmail.com>
2020-11-10 00:45:43 -0800

    Added actions and reducers for pokemon

---
+++ src/Store.ts
@@ -0,0 +1,8 @@
+import {applyMiddleware, compose, createStore} from 'redux';
+import RootReducer from './reducers/RootReducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const Store = createStore(RootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default Store;
---
+++ src/reducers/PokemonReducer.ts
@@ -0,0 +1,13 @@
+interface DefaultStateI {
+    
+}
+
+const defaultState: DefaultStateI = {
+
+}
+
+const pokemonReducer = (state: DefaultStateI, action: any): DefaultStateI => {
+    return state;
+}
+
+export default pokemonReducer
