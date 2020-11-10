494277de1904c09ec2b66d4e9f06b14360ad391a
Tin La <tinla14@gmail.com>
2020-11-10 01:06:10 -0800

    Added search pokemon and display result

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {RootStore} from './Store';
+import {GetPokemon} from './actions/PokemonActions';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  const [pokemonName, setPokemonName] = useState("");
+  const pokemonState = useSelector((state: RootStore) => state.pokemon);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => setPokemonName(event.target.value);
+
+  const handleSubmit = () => dispatch(GetPokemon(pokemonName));
+
   return (
     <div className="App">
-      
+      <input 
+        type="text" 
+        placeholder="Search Pokemon"
+        onChange={handleChange}/>
+      <button onClick={handleSubmit}>Search</button>
+      {pokemonState.pokemon ? (
+        <div>
+          <img src={pokemonState.pokemon.sprites.front_default} alt="pokemon photo"/>
+          {pokemonState.pokemon.abilities.map(ability => {
+            return <p>{ability.ability.name}</p>
+          })}
+        </div>
+      ) : <p>Pokemon is not found!!!</p>}
     </div>
   );
 }
